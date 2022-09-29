48644f05441bbd07b0f233fe1c5f26d76fc3a5b8
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-09-29 13:34:36 +0000

    2022/09/29

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,33 +1,25 @@
 class Admin::ItemsController < ApplicationController
-
   def create
     @item=Item.new(item_params)
     @item.save
     redirect_to admin_item_path(@item.id)
   end
-
   def new
     @item=Item.new
-
-
   end
-
   def index
-    @items=Item.all
-
+    @items=Item.page(params[:page])
   end
-
   def show
     @item= Item.find(params[:id])
   end
-
   def edit
     @item= Item.find(params[:id])
   end
-
   def update
    @item = Item.find(params[:id])
    @item.update(item_params)
+   redirect_to admin_item_path(@item.id)
   end
 
   private
