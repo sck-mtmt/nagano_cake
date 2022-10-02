dee72aedfe65f37e118eff4d7df1b9a6b319132c
sck-mtmt <108612924+sck-mtmt@users.noreply.github.com>
2022-10-02 15:04:37 +0000

    2022/10/02

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,25 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers=Customer.page(params[:page])
+      @customers=Customer.page(params[:page])
   end
 
   def show
-    @customer= Customer.find(params[:id])
+    @customer= current_customer
   end
 
   def edit
+    @customer= current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
 
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email,
+                                     :encrypted_password, :postal_code, :address, :telephone_namber, :is_deleted)
+  end
 end
